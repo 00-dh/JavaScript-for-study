ec961e1d33d8d7e5e147b169130405d458a2631d
00-dh <kimdh081599@gmail.com>
2022-08-05 19:09:34 +0900

    curt code about conditional (if else)

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 const h2 = document.querySelector("h2");
 const body = document.querySelector("body");
 
-function handleResize() {
-  let bodyWidth = window.innerWidth;
-  if (bodyWidth >= 500 && bodyWidth < 813) {
+function addAndRemoveEditor(color) {
+  if (color === "blue") {
     body.classList.add("blue");
     body.classList.remove("purple");
     body.classList.remove("yellow");
-  } else if (bodyWidth >= 813 && bodyWidth < 1126) {
+  } else if (color === "purple") {
     body.classList.add("purple");
     body.classList.remove("yellow");
     body.classList.remove("blue");
-  } else if (1126 <= bodyWidth) {
+  } else if (color === "yellow") {
     body.classList.add("yellow");
     body.classList.remove("purple");
     body.classList.remove("blue");
+  }
+}
+
+function handleResize() {
+  let bodyWidth = window.innerWidth;
+  if (bodyWidth >= 500 && bodyWidth < 813) {
+    addAndRemoveEditor("blue");
+  } else if (bodyWidth >= 813 && bodyWidth < 1126) {
+    addAndRemoveEditor("purple");
+  } else if (1126 <= bodyWidth) {
+    addAndRemoveEditor("yellow");
   }
 }
 
