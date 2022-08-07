9b91b2863211fd44c5d484b4f96feb88712a2b58
00-dh <kimdh081599@gmail.com>
2022-08-07 20:15:21 +0900

    make code simply

--- app.js
+++ app.js
@@ -1,33 +1,50 @@
 const body = document.querySelector("body");
 const WidthValue = document.querySelector(".width_value");
 
-function colorEditor(colorName) {
-  if (colorName === "blue") {
+const colorEditor = {
+  addBlue: function () {
+    body.className = "";
     body.classList.add("blue");
-    body.classList.remove("purple");
-    body.classList.remove("yellow");
-  } else if (colorName === "purple") {
+  },
+  addPurple: function () {
+    body.className = "";
     body.classList.add("purple");
-    body.classList.remove("yellow");
-    body.classList.remove("blue");
-  } else if (colorName === "yellow") {
+  },
+  addYellow: function () {
+    body.className = "";
     body.classList.add("yellow");
-    body.classList.remove("purple");
-    body.classList.remove("blue");
-  }
-}
+  },
+  addTomato: function () {
+    body.className = "";
+    body.classList.add("tomato");
+  },
+};
 
 function handleResize() {
   let bodyWidth = window.innerWidth;
   WidthValue.innerText = `You're Width length is now ${bodyWidth}`;
   if (bodyWidth >= 500 && bodyWidth < 813) {
-    colorEditor("blue");
+    colorEditor.addBlue();
+    return "blue";
   } else if (bodyWidth >= 813 && bodyWidth < 1126) {
-    colorEditor("purple");
+    colorEditor.addPurple();
+    return "purple";
   } else if (1126 <= bodyWidth) {
-    colorEditor("yellow");
+    colorEditor.addYellow();
+    return "yellow";
   }
 }
 
+function handleOffWifi() {
+  colorEditor.addTomato();
+  alert("Your wifi is disconnected");
+}
+
+function handleOnWifi() {
+  alert("Your wifi is connected");
+}
+
 handleResize();
+window.addEventListener("offline", handleOffWifi);
+window.addEventListener("online", handleOnWifi);
 window.addEventListener("resize", handleResize);
