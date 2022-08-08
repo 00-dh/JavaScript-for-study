5ab8e8dd1dcb97481a0857b21ba335a62dd2fb92
00-dh <kimdh081599@gmail.com>
2022-08-08 21:41:42 +0900

    add win & lose count

--- app.js
+++ app.js
@@ -3,21 +3,32 @@ const GenerateNumber = document.querySelector(".generate_nb input");
 const GuessNumber = document.querySelector(".guess_nb input");
 const resultMessage = document.querySelector(".message");
 const winOrLose = document.querySelector(".win_or_lose");
+const count = document.querySelector(".win_or_lose__count");
+
+let winCount = 0;
+let loseCount = 0;
 
 function handleClickPlay(event) {
-  const machineNumber = Math.round(Math.random() * GenerateNumber.value);
   event.preventDefault();
-  if (GuessNumber.value > GenerateNumber.value) {
+  const GuessValue = parseInt(GuessNumber.value, 10);
+  const GenerateValue = parseInt(GenerateNumber.value, 10);
+  const machineNumber = Math.round(Math.random() * GenerateValue);
+
+  if (GuessValue > GenerateValue) {
     resultMessage.innerText =
       "please check your number.\n Isn't Guess number bigger than Generate number?";
     winOrLose.innerText = "";
+    return 0;
   }
-  resultMessage.innerText = `You chose: ${GuessNumber.value}, the machine chose: ${machineNumber}`;
-  if (parseInt(GuessNumber.value, 10) === machineNumber) {
+  resultMessage.innerText = `You chose: ${GuessValue}, the machine chose: ${machineNumber}`;
+  if (parseInt(GuessValue, 10) === machineNumber) {
     winOrLose.innerText = "You win!";
+    winCount += 1;
   } else {
     winOrLose.innerText = "You Lose!";
+    loseCount += 1;
   }
+  count.innerText = `win: ${winCount} times\n lose: ${loseCount} times`;
 }
 
 playEvent.addEventListener("submit", handleClickPlay);
