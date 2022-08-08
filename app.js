061f6737f69604f25296a438ddbe0bd1bb807365
00-dh <kimdh081599@gmail.com>
2022-08-08 17:11:51 +0900

    learn about Math.random function in advanced

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
-const GenerateInput = document.querySelector(".generate_nb input");
-const GuessInput = document.querySelector(".guess_nb input");
+const playEvent = document.querySelector("form");
+const GenerateNumber = document.querySelector(".generate_nb input");
+const GuessNumber = document.querySelector(".guess_nb input");
+const resultMessage = document.querySelector(".message");
+const winOrLose = document.querySelector(".win_or_lose");
+
+function handleClickPlay(event) {
+  event.preventDefault();
+  resultMessage.innerText = `You chose: ${
+    GuessNumber.value
+  }, the machine chose: ${Math.round(Math.random() * GenerateNumber.value)}`;
+  if (
+    parseInt(GuessNumber.value) ===
+    Math.round(Math.random() * GenerateNumber.value)
+  ) {
+    winOrLose.innerText = "You win!";
+  } else {
+    winOrLose.innerText = "You Lose";
+  }
+}
+
+playEvent.addEventListener("submit", handleClickPlay);
