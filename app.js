1901e57a2acb32818c80bc8a8f02ba6b0c942b23
00-dh <kimdh081599@gmail.com>
2022-08-08 20:38:13 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
-//fot addEventListener submit
 const playEvent = document.querySelector("form");
-
-//for get value of input
 const GenerateNumber = document.querySelector(".generate_nb input");
 const GuessNumber = document.querySelector(".guess_nb input");
-
-//for Element.innerText
 const resultMessage = document.querySelector(".message");
 const winOrLose = document.querySelector(".win_or_lose");
 
@@ -15,17 +10,13 @@ function handleClickPlay(event) {
   if (GuessNumber.value > GenerateNumber.value) {
     resultMessage.innerText =
       "please check your number.\n Isn't Guess number bigger than Generate number?";
-    resultMessage.innerText = `You chose: ${GuessNumber.value}, the machine chose: ${machineNumber}`;
-    if (parseInt(GuessNumber.value) === machineNumber) {
-      winOrLose.innerText = "You win!";
-    } else {
-      resultMessage.innerText = `You chose: ${GuessNumber.value}, the machine chose: ${machineNumber}`;
-      if (parseInt(GuessNumber.value) === machineNumber) {
-        winOrLose.innerText = "You win!";
-      } else {
-        winOrLose.innerText = "You Lose!";
-      }
-    }
+    winOrLose.innerText = "";
+  }
+  resultMessage.innerText = `You chose: ${GuessNumber.value}, the machine chose: ${machineNumber}`;
+  if (parseInt(GuessNumber.value, 10) === machineNumber) {
+    winOrLose.innerText = "You win!";
+  } else {
+    winOrLose.innerText = "You Lose!";
   }
 }
 
