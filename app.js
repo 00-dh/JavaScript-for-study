9dab24a45ab4a05ee5f25cf0a5a28553f1cd9557
00-dh <kimdh081599@gmail.com>
2022-08-08 18:46:26 +0900

    add caution text (if guess number bigger than generate number)

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const winOrLose = document.querySelector(".win_or_lose");
 function handleClickPlay(event) {
   const machineNumber = Math.round(Math.random() * GenerateNumber.value);
   event.preventDefault();
+  if (GuessNumber.value > GenerateNumber.value) {
+    resultMessage.innerText =
+      "please check your number.\n Isn't Guess number bigger than Generate number?";
+    return 0;
+  }
   resultMessage.innerText = `You chose: ${GuessNumber.value}, the machine chose: ${machineNumber}`;
   if (parseInt(GuessNumber.value) === machineNumber) {
     winOrLose.innerText = "You win!";
