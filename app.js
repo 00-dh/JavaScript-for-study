9e6b4b7348916edbea220deeeff769dc11f7fb68
00-dh <kimdh081599@gmail.com>
2022-08-08 20:32:47 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+//fot addEventListener submit
 const playEvent = document.querySelector("form");
+
+//for get value of input
 const GenerateNumber = document.querySelector(".generate_nb input");
 const GuessNumber = document.querySelector(".guess_nb input");
+
+//for Element.innerText
 const resultMessage = document.querySelector(".message");
 const winOrLose = document.querySelector(".win_or_lose");
 
@@ -10,13 +15,13 @@ function handleClickPlay(event) {
   if (GuessNumber.value > GenerateNumber.value) {
     resultMessage.innerText =
       "please check your number.\n Isn't Guess number bigger than Generate number?";
-    return 0;
-  }
-  resultMessage.innerText = `You chose: ${GuessNumber.value}, the machine chose: ${machineNumber}`;
-  if (parseInt(GuessNumber.value) === machineNumber) {
-    winOrLose.innerText = "You win!";
   } else {
-    winOrLose.innerText = "You Lose!";
+    resultMessage.innerText = `You chose: ${GuessNumber.value}, the machine chose: ${machineNumber}`;
+    if (parseInt(GuessNumber.value) === machineNumber) {
+      winOrLose.innerText = "You win!";
+    } else {
+      winOrLose.innerText = "You Lose!";
+    }
   }
 }
 
