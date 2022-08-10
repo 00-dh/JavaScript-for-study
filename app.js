d3d78971bd78da4bb0503d6b8c30f64ea0235123
00-dh <kimdh081599@gmail.com>
2022-08-10 17:41:23 +0900

    add .padStart(2, "0") in separate time

--- app.js
+++ app.js
@@ -3,19 +3,21 @@ const clock = document.querySelector("#clock");
 function nowTime() {
   const nowDate = new Date();
   const D_dayChristmas = new Date("12 / 25 / 2022");
-  const nowDates = nowDate.getDate();
-  const nowHours = nowDate.getHours();
-  const nowMinutes = nowDate.getMinutes();
-  const nowSeconds = nowDate.getSeconds();
-  clock.innerText = `${nowDates}d ${nowHours}h ${nowMinutes}m ${nowSeconds}s`;
   const diffTime = D_dayChristmas - nowDate;
-  const diffDays = diffTime / (1000 * 60 * 60 * 24);
-  const diffHours = (diffTime / (1000 * 60 * 60)) % 24;
-  const diffMinutes = (diffTime / (1000 * 60)) % 60;
-  const diffSeconds = (diffTime / 1000) % 60;
-  clock.innerText = `${Math.floor(diffDays)}d ${Math.floor(
-    diffHours
-  )}h ${Math.floor(diffMinutes)}m ${Math.floor(diffSeconds)}s`;
+  const diffDays = String(
+    Math.floor(diffTime / (1000 * 60 * 60 * 24))
+  ).padStart(2, "0");
+  const diffHours = String(
+    Math.floor((diffTime / (1000 * 60 * 60)) % 24)
+  ).padStart(2, "0");
+  const diffMinutes = String(
+    Math.floor((diffTime / (1000 * 60)) % 60)
+  ).padStart(2, "0");
+  const diffSeconds = String(Math.floor((diffTime / 1000) % 60)).padStart(
+    2,
+    "0"
+  );
+  clock.innerText = `${diffDays}d ${diffHours}h ${diffMinutes}m ${diffSeconds}s`;
 }
 
 nowTime();
