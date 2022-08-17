846abcdb81bfb38ea791fb02c4d50930e0339f27
00-dh <kimdh081599@gmail.com>
2022-08-17 18:38:38 +0900

    ✂️Rename: linear-gradient files

---
+++ linear-gradient/linear-gradient.js
@@ -0,0 +1,37 @@
+const colors = [
+  "#ef5777",
+  "#575fcf",
+  "#4bcffa",
+  "#34e7e4",
+  "#0be881",
+  "#f53b57",
+  "#3c40c6",
+  "#0fbcf9",
+  "#00d8d6",
+  "#05c46b",
+  "#ffc048",
+  "#ffdd59",
+  "#ff5e57",
+  "#d2dae2",
+  "#485460",
+  "#ffa801",
+  "#ffd32a",
+  "#ff3f34",
+];
+
+const btn = document.querySelector("button");
+
+function change_color() {
+  const randomValue1 = Math.floor(Math.random() * colors.length);
+  let randomValue2 = Math.floor(Math.random() * colors.length);
+  if (randomValue1 === randomValue2) {
+    while (randomValue1 === randomValue2) {
+      randomValue2 = Math.floor(Math.random() * colors.length);
+    }
+  }
+  const randomColor1 = colors[randomValue1];
+  const randomColor2 = colors[randomValue2];
+  document.body.style.backgroundImage = `linear-gradient(to right, ${randomColor1}, ${randomColor2})`;
+}
+
+btn.addEventListener("click", change_color);
