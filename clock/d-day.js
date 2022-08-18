e3cb47caffb8d6ab3f8a08785b2b4696523aaa65
00-dh <kimdh081599@gmail.com>
2022-08-18 14:33:31 +0900

    ✒️Feat: Added D-day calculator

---
+++ clock/d-day.js
@@ -0,0 +1,20 @@
+const D_day = document.querySelector(".d-day_1");
+const D_day2 = document.querySelector(".d-day_2");
+const form = document.querySelector("form");
+const result_d_day = document.querySelector(".final_d-day");
+
+function calculator(event) {
+  event.preventDefault();
+  const firstD_day = D_day.valueAsNumber;
+  const secondD_day = D_day2.valueAsNumber;
+  const finalD_day = (secondD_day - firstD_day) / (1000 * 60 * 60 * 24);
+  let string = "";
+  if (finalD_day > 0) {
+    string = " 남았습니다.";
+  } else if (finalD_day < 0) {
+    string = " 지났습니다.";
+  }
+  result_d_day.innerText = `${Math.abs(finalD_day)}일 ${string}`;
+}
+
+form.addEventListener("submit", calculator);
