f82d733939df43b44ee929fe8d55dad9bce9ec29
00-dh <kimdh081599@gmail.com>
2022-08-21 16:38:55 +0900

    ✒️Feat: add & delete function in list

--- todo_list/todo_list.js
+++ todo_list/todo_list.js
@@ -1,9 +1,29 @@
 const writeForm = document.querySelector(".todo_input");
 const writeInput = document.querySelector(".todo_input input");
+const todoList = document.querySelector(".todo_list");
 
 function inputValue(event) {
   event.preventDefault();
-  console.log(writeInput.value);
+  const input_value = writeInput.value;
+  paintTodo(input_value);
+  writeInput.value = "";
+}
+
+function paintTodo(todo) {
+  const li = document.createElement("li");
+  todoList.appendChild(li);
+  const span = document.createElement("span");
+  span.innerText = todo;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteTodo);
+  li.appendChild(span);
+  li.appendChild(button);
+}
+
+function deleteTodo(event) {
+  const li = event.target.parentNode;
+  li.remove();
 }
 
 writeForm.addEventListener("submit", inputValue);
