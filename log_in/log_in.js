eb1b9517f6f20630736567ef32a32aa4844474be
00-dh <kimdh081599@gmail.com>
2022-08-25 14:47:27 +0900

    ✒️Feat: build hidden function when user log in & out

--- log_in/log_in.js
+++ log_in/log_in.js
@@ -1,8 +1,11 @@
 const logInForm = document.querySelector(".log_in");
+const logOutForm = document.querySelector(".log_out");
 const Id = logInForm.querySelector(".id");
 const passWord = logInForm.querySelector(".pw");
 
 const greeting = document.querySelector(".hello_user");
+
+const CLASSNAME_HIDDEN = "hidden";
 
 function sayHello(event) {
   event.preventDefault();
@@ -10,6 +13,15 @@ function sayHello(event) {
   Id.value = "";
   passWord.value = "";
   greeting.innerText = `hello. ${userId}`;
+  logInForm.classList.add(CLASSNAME_HIDDEN);
+  logOutForm.classList.remove(CLASSNAME_HIDDEN);
+}
+
+function logOut() {
+  logInForm.classList.remove(CLASSNAME_HIDDEN);
+  logOutForm.classList.add(CLASSNAME_HIDDEN);
+  greeting.innerText = "";
 }
 
 logInForm.addEventListener("submit", sayHello);
+logOutForm.addEventListener("submit", logOut);
