ffc9bd322ae58c515d70365e0ce7a0e7acce67cf
00_kdh <98944423+00-dh@users.noreply.github.com>
2022-08-29 15:09:06 +0900

    ✒️Feat: added military d-day calculate function

--- clock/gundol.js
+++ clock/gundol.js
@@ -1,2 +1,13 @@
 const milTime = document.querySelector(".military_time");
-const enlist = milTime.querySelector("");
+const enlist = milTime.querySelector(".enlist");
+const disCharge = milTime.querySelector(".discharge");
+const milD_day = milTime.querySelector(".mil__D-day");
+
+function sayHello(event) {
+  event.preventDefault();
+  const milD_dayValue =
+    (disCharge.valueAsNumber - enlist.valueAsNumber) / (1000 * 60 * 60 * 24);
+  milD_day.innerText = milD_dayValue;
+}
+
+milTime.addEventListener("submit", sayHello);
